dea3ce711aee6cdc4b55c949bac86d18e7aa1c4c
mrenaldyr <mrenaldyr03@gmail.com>
2023-09-08 20:26:26 +0700

    feat: add aos, fix responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,14 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 import typed from './typed'
 
+AOS.init()
+
 const header = document.querySelector('#header')
+const button_nav = document.querySelector('#btn-mobile-nav')
+const menu_list = document.querySelector('#mobile-menu')
+const menu_icon = document.querySelector('#menu-icon')
+const close_icon = document.querySelector('#close-icon')
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
@@ -8,4 +16,10 @@ window.addEventListener('scroll', () => {
   } else {
     header.classList.remove('scroll-header') // Reset to transparent if not scrolled
   }
+})
+
+button_nav.addEventListener('click', () => {
+  menu_list.classList.toggle('!-left-full')
+  menu_icon.classList.toggle('hidden')
+  close_icon.classList.toggle('hidden')
 })
